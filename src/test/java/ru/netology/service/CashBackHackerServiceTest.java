0e4cf84cb122729885ea963106f7ae5e1e063bc0
Regie <reginapanina@mail.ru>
2023-04-25 12:05:43 +0300

    Bug

---
+++ src/test/java/ru/netology/service/CashBackHackerServiceTest.java
@@ -0,0 +1,46 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashBackHackerServiceTest {
+    CashBackHackerService service = new CashBackHackerService();
+    @Test
+    public void calcRemainIfAmountUnderBoundary() {
+        int amount = 500;
+
+        int expected = 500;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void calcRemainIfAmountOverBoundary() {
+        int amount = 1500;
+
+        int expected = 500;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calcRemainIfAmountEqualToBoundary() {
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calcRemainIfAmountEqualToZero() {
+        int amount = 0;
+
+        int expected = 1000;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+
+}
